bf5c95119e9a9dbceaf2203bf48455d6b20dd804
Natalia Canudas <natalia.200351@hotmail.com>
2021-10-22 11:10:38 +0200

    OCT 22

--- Practica5/Practica5/Form1.Designer.vb
+++ Practica5/Practica5/Form1.Designer.vb
@@ -24,10 +24,17 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.Ltbx_listaIzq = New System.Windows.Forms.ListBox()
         Me.Txtb_EntradaListIzq = New System.Windows.Forms.TextBox()
-        Me.Btn_borr_lista = New System.Windows.Forms.Button()
+        Me.Btn_borr_listaIzq = New System.Windows.Forms.Button()
         Me.Btn_borr_listaCentro = New System.Windows.Forms.Button()
         Me.Txtb_EntradaListCentro = New System.Windows.Forms.TextBox()
         Me.Ltbx_listaCentro = New System.Windows.Forms.ListBox()
+        Me.Btn_borr_listaDecha = New System.Windows.Forms.Button()
+        Me.Txtb_EntradaListDecha = New System.Windows.Forms.TextBox()
+        Me.Ltbx_listaDecha = New System.Windows.Forms.ListBox()
+        Me.Btn_IzqACentro = New System.Windows.Forms.Button()
+        Me.Btn_CentroAIzq = New System.Windows.Forms.Button()
+        Me.Btn_DechaACentro = New System.Windows.Forms.Button()
+        Me.Btn_CentroADecha = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Ltbx_listaIzq
@@ -35,31 +42,31 @@ Partial Class Form1
         Me.Ltbx_listaIzq.FormattingEnabled = True
         Me.Ltbx_listaIzq.ItemHeight = 15
         Me.Ltbx_listaIzq.Items.AddRange(New Object() {"Ford", "Mazda", "Seat", "Audi", "Mercedes"})
-        Me.Ltbx_listaIzq.Location = New System.Drawing.Point(99, 138)
+        Me.Ltbx_listaIzq.Location = New System.Drawing.Point(78, 138)
         Me.Ltbx_listaIzq.Name = "Ltbx_listaIzq"
-        Me.Ltbx_listaIzq.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.Ltbx_listaIzq.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
         Me.Ltbx_listaIzq.Size = New System.Drawing.Size(120, 154)
         Me.Ltbx_listaIzq.TabIndex = 0
         '
         'Txtb_EntradaListIzq
         '
-        Me.Txtb_EntradaListIzq.Location = New System.Drawing.Point(99, 309)
+        Me.Txtb_EntradaListIzq.Location = New System.Drawing.Point(78, 309)
         Me.Txtb_EntradaListIzq.Name = "Txtb_EntradaListIzq"
         Me.Txtb_EntradaListIzq.Size = New System.Drawing.Size(120, 23)
         Me.Txtb_EntradaListIzq.TabIndex = 1
         '
-        'Btn_borr_lista
+        'Btn_borr_listaIzq
         '
-        Me.Btn_borr_lista.Location = New System.Drawing.Point(99, 100)
-        Me.Btn_borr_lista.Name = "Btn_borr_lista"
-        Me.Btn_borr_lista.Size = New System.Drawing.Size(120, 23)
-        Me.Btn_borr_lista.TabIndex = 2
-        Me.Btn_borr_lista.Text = "Borrar lista"
-        Me.Btn_borr_lista.UseVisualStyleBackColor = True
+        Me.Btn_borr_listaIzq.Location = New System.Drawing.Point(78, 100)
+        Me.Btn_borr_listaIzq.Name = "Btn_borr_listaIzq"
+        Me.Btn_borr_listaIzq.Size = New System.Drawing.Size(120, 23)
+        Me.Btn_borr_listaIzq.TabIndex = 2
+        Me.Btn_borr_listaIzq.Text = "Borrar lista"
+        Me.Btn_borr_listaIzq.UseVisualStyleBackColor = True
         '
         'Btn_borr_listaCentro
         '
-        Me.Btn_borr_listaCentro.Location = New System.Drawing.Point(289, 100)
+        Me.Btn_borr_listaCentro.Location = New System.Drawing.Point(335, 100)
         Me.Btn_borr_listaCentro.Name = "Btn_borr_listaCentro"
         Me.Btn_borr_listaCentro.Size = New System.Drawing.Size(120, 23)
         Me.Btn_borr_listaCentro.TabIndex = 5
@@ -68,7 +75,7 @@ Partial Class Form1
         '
         'Txtb_EntradaListCentro
         '
-        Me.Txtb_EntradaListCentro.Location = New System.Drawing.Point(289, 309)
+        Me.Txtb_EntradaListCentro.Location = New System.Drawing.Point(335, 309)
         Me.Txtb_EntradaListCentro.Name = "Txtb_EntradaListCentro"
         Me.Txtb_EntradaListCentro.Size = New System.Drawing.Size(120, 23)
         Me.Txtb_EntradaListCentro.TabIndex = 4
@@ -78,21 +85,91 @@ Partial Class Form1
         Me.Ltbx_listaCentro.FormattingEnabled = True
         Me.Ltbx_listaCentro.ItemHeight = 15
         Me.Ltbx_listaCentro.Items.AddRange(New Object() {"Ford", "Mazda", "Seat", "Audi", "Mercedes"})
-        Me.Ltbx_listaCentro.Location = New System.Drawing.Point(289, 138)
+        Me.Ltbx_listaCentro.Location = New System.Drawing.Point(335, 138)
         Me.Ltbx_listaCentro.Name = "Ltbx_listaCentro"
-        Me.Ltbx_listaCentro.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
+        Me.Ltbx_listaCentro.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
         Me.Ltbx_listaCentro.Size = New System.Drawing.Size(120, 154)
         Me.Ltbx_listaCentro.TabIndex = 3
+        '
+        'Btn_borr_listaDecha
+        '
+        Me.Btn_borr_listaDecha.Location = New System.Drawing.Point(596, 100)
+        Me.Btn_borr_listaDecha.Name = "Btn_borr_listaDecha"
+        Me.Btn_borr_listaDecha.Size = New System.Drawing.Size(120, 23)
+        Me.Btn_borr_listaDecha.TabIndex = 8
+        Me.Btn_borr_listaDecha.Text = "Borrar lista"
+        Me.Btn_borr_listaDecha.UseVisualStyleBackColor = True
+        '
+        'Txtb_EntradaListDecha
+        '
+        Me.Txtb_EntradaListDecha.Location = New System.Drawing.Point(596, 309)
+        Me.Txtb_EntradaListDecha.Name = "Txtb_EntradaListDecha"
+        Me.Txtb_EntradaListDecha.Size = New System.Drawing.Size(120, 23)
+        Me.Txtb_EntradaListDecha.TabIndex = 7
+        '
+        'Ltbx_listaDecha
+        '
+        Me.Ltbx_listaDecha.FormattingEnabled = True
+        Me.Ltbx_listaDecha.ItemHeight = 15
+        Me.Ltbx_listaDecha.Items.AddRange(New Object() {"Ford", "Mazda", "Seat", "Audi", "Mercedes"})
+        Me.Ltbx_listaDecha.Location = New System.Drawing.Point(596, 138)
+        Me.Ltbx_listaDecha.Name = "Ltbx_listaDecha"
+        Me.Ltbx_listaDecha.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple
+        Me.Ltbx_listaDecha.Size = New System.Drawing.Size(120, 154)
+        Me.Ltbx_listaDecha.TabIndex = 6
+        '
+        'Btn_IzqACentro
+        '
+        Me.Btn_IzqACentro.Location = New System.Drawing.Point(230, 168)
+        Me.Btn_IzqACentro.Name = "Btn_IzqACentro"
+        Me.Btn_IzqACentro.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_IzqACentro.TabIndex = 9
+        Me.Btn_IzqACentro.Text = "---->"
+        Me.Btn_IzqACentro.UseVisualStyleBackColor = True
+        '
+        'Btn_CentroAIzq
+        '
+        Me.Btn_CentroAIzq.Location = New System.Drawing.Point(230, 224)
+        Me.Btn_CentroAIzq.Name = "Btn_CentroAIzq"
+        Me.Btn_CentroAIzq.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_CentroAIzq.TabIndex = 10
+        Me.Btn_CentroAIzq.Text = "<----"
+        Me.Btn_CentroAIzq.UseVisualStyleBackColor = True
+        '
+        'Btn_DechaACentro
+        '
+        Me.Btn_DechaACentro.Location = New System.Drawing.Point(492, 224)
+        Me.Btn_DechaACentro.Name = "Btn_DechaACentro"
+        Me.Btn_DechaACentro.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_DechaACentro.TabIndex = 12
+        Me.Btn_DechaACentro.Text = "<----"
+        Me.Btn_DechaACentro.UseVisualStyleBackColor = True
+        '
+        'Btn_CentroADecha
+        '
+        Me.Btn_CentroADecha.Location = New System.Drawing.Point(492, 168)
+        Me.Btn_CentroADecha.Name = "Btn_CentroADecha"
+        Me.Btn_CentroADecha.Size = New System.Drawing.Size(75, 23)
+        Me.Btn_CentroADecha.TabIndex = 11
+        Me.Btn_CentroADecha.Text = "---->"
+        Me.Btn_CentroADecha.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Btn_DechaACentro)
+        Me.Controls.Add(Me.Btn_CentroADecha)
+        Me.Controls.Add(Me.Btn_CentroAIzq)
+        Me.Controls.Add(Me.Btn_IzqACentro)
+        Me.Controls.Add(Me.Btn_borr_listaDecha)
+        Me.Controls.Add(Me.Txtb_EntradaListDecha)
+        Me.Controls.Add(Me.Ltbx_listaDecha)
         Me.Controls.Add(Me.Btn_borr_listaCentro)
         Me.Controls.Add(Me.Txtb_EntradaListCentro)
         Me.Controls.Add(Me.Ltbx_listaCentro)
-        Me.Controls.Add(Me.Btn_borr_lista)
+        Me.Controls.Add(Me.Btn_borr_listaIzq)
         Me.Controls.Add(Me.Txtb_EntradaListIzq)
         Me.Controls.Add(Me.Ltbx_listaIzq)
         Me.Name = "Form1"
@@ -104,8 +181,15 @@ Partial Class Form1
 
     Friend WithEvents Ltbx_listaIzq As ListBox
     Friend WithEvents Txtb_EntradaListIzq As TextBox
-    Friend WithEvents Btn_borr_lista As Button
+    Friend WithEvents Btn_borr_listaIzq As Button
     Friend WithEvents Btn_borr_listaCentro As Button
     Friend WithEvents Txtb_EntradaListCentro As TextBox
     Friend WithEvents Ltbx_listaCentro As ListBox
+    Friend WithEvents Btn_borr_listaDecha As Button
+    Friend WithEvents Txtb_EntradaListDecha As TextBox
+    Friend WithEvents Ltbx_listaDecha As ListBox
+    Friend WithEvents Btn_IzqACentro As Button
+    Friend WithEvents Btn_CentroAIzq As Button
+    Friend WithEvents Btn_DechaACentro As Button
+    Friend WithEvents Btn_CentroADecha As Button
 End Class
--- Practica5/Practica5/Form1.vb
+++ Practica5/Practica5/Form1.vb
@@ -9,13 +9,15 @@
 
     Private Sub InicializarControles()
         Txtb_EntradaListIzq.Tag = Ltbx_listaIzq
-        Btn_borr_lista.Tag = Ltbx_listaIzq
+        Btn_borr_listaIzq.Tag = Ltbx_listaIzq
         Txtb_EntradaListCentro.Tag = Ltbx_listaCentro
         Btn_borr_listaCentro.Tag = Ltbx_listaCentro
+        Txtb_EntradaListDecha.Tag = Ltbx_listaDecha
+        Btn_borr_listaDecha.Tag = Ltbx_listaDecha
     End Sub
 
     Private Sub Txtb_EntradaListIzq_KeyPress(sender As Object, e As KeyPressEventArgs) _
-                Handles Txtb_EntradaListIzq.KeyPress, Txtb_EntradaListCentro.KeyPress
+                Handles Txtb_EntradaListIzq.KeyPress, Txtb_EntradaListCentro.KeyPress, Txtb_EntradaListDecha.KeyPress
 
         Dim MiTexto As TextBox = sender
         Dim MiListaEntrada As ListBox = MiTexto.Tag
@@ -30,7 +32,7 @@
     End Sub
 
     Private Sub Btn_borr_lista_Click(sender As Object, e As EventArgs) _
-                Handles Btn_borr_lista.Click, Btn_borr_listaCentro.Click
+                Handles Btn_borr_listaIzq.Click, Btn_borr_listaCentro.Click, Btn_borr_listaDecha.Click
         Dim MiBoton As Button = sender
         Dim MiLista As ListBox = MiBoton.Tag
         MiLista.Items.Clear()
@@ -38,23 +40,49 @@
 
     Private Sub IntercambioListas()
         Do While MiListaOrigen.SelectedItems.Count
+            If Not MiListaDestino.Items.IndexOf(MiListaOrigen.SelectedItems(0)) = -1 Then Exit Sub
             MiListaDestino.Items.Add(MiListaOrigen.SelectedItems(0))
-            MiListaOrigen.Items.Remove(MiListaDestino.SelectedItems(0))
+            MiListaOrigen.Items.Remove(MiListaOrigen.SelectedItems(0))
         Loop
 
     End Sub
 
+    'Se envia un elemento haciendo doble-click en él
     Private Sub Ltbx_listaIzq_DoubleClick(sender As Object, e As EventArgs) _
-                Handles Ltbx_listaIzq.DoubleClick, Ltbx_listaCentro.DoubleClick
+                Handles Ltbx_listaIzq.DoubleClick, Ltbx_listaCentro.DoubleClick, Ltbx_listaDecha.DoubleClick
         Dim MiLista As ListBox = sender
-        If MiLista.SelectedItem.count = 0 Then Exit Sub
-        Select Case MiLista.Name.ToString
+        If MiLista.SelectedItems.Count = 0 Then Exit Sub
+        Select Case MiLista.Name
             Case "Ltbx_listaIzq"
                 MiListaOrigen = Ltbx_listaIzq
                 MiListaDestino = Ltbx_listaCentro
             Case "Ltbx_listaCentro"
                 MiListaOrigen = Ltbx_listaCentro
+                MiListaDestino = Ltbx_listaDecha
+            Case "Ltbx_listaDecha"
+                MiListaOrigen = Ltbx_listaDecha
                 MiListaDestino = Ltbx_listaIzq
+        End Select
+        IntercambioListas()
+    End Sub
+
+    'Se envian elementos selecionados mediante los botones
+    Private Sub Btn_Enviar_Click(sender As Object, e As EventArgs) _
+        Handles Btn_IzqACentro.Click, Btn_CentroAIzq.Click, Btn_DechaACentro.Click, Btn_CentroADecha.Click
+        Dim MiBoton As Button = sender
+        Select Case MiBoton.Name
+            Case "Btn_IzqACentro"
+                MiListaOrigen = Ltbx_listaIzq
+                MiListaDestino = Ltbx_listaCentro
+            Case "Btn_CentroAIzq"
+                MiListaOrigen = Ltbx_listaCentro
+                MiListaDestino = Ltbx_listaIzq
+            Case "Btn_DechaACentro"
+                MiListaOrigen = Ltbx_listaDecha
+                MiListaDestino = Ltbx_listaCentro
+            Case "Btn_CentroADecha"
+                MiListaOrigen = Ltbx_listaCentro
+                MiListaDestino = Ltbx_listaDecha
         End Select
         IntercambioListas()
     End Sub
