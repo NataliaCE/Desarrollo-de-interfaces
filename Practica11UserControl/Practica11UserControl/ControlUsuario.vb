e242082af0af6f02135b6cdcee58a1be965917de
Natalia Canudas <natalia.200351@hotmail.com>
2021-11-26 09:00:25 +0100

    Control de usuario

---
+++ Practica11UserControl/Practica11UserControl/ControlUsuario.vb
@@ -0,0 +1,26 @@
+﻿Imports System.ComponentModel
+Public Class ControlUsuario
+
+    Private Texto As String
+
+    'Con esta propiedad se puede acceder a la variable Texto que es privada
+    <Description("Propiedad de texto")>
+    Public Property TextoInicial() As String
+        'Getter
+        Get
+            Return Texto
+        End Get
+        'Setter
+        Set(value As String)
+            Texto = value
+        End Set
+    End Property
+
+    Private Sub MiBoton_Click(sender As Object, e As EventArgs) Handles MiBoton.Click
+        MiLista.Items.Add(MiTexto.Text)
+    End Sub
+
+    Private Sub ControlUsuario_Load(sender As Object, e As EventArgs) Handles Me.Load
+        MiTexto.Text = Me.TextoInicial
+    End Sub
+End Class
---
+++ Practica11UserControl/Practica11UserControl/Form1.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.MiControl = New Practica11UserControl.ControlUsuario()
+        Me.SuspendLayout()
+        '
+        'MiControl
+        '
+        Me.MiControl.Location = New System.Drawing.Point(248, 87)
+        Me.MiControl.Name = "MiControl"
+        Me.MiControl.Size = New System.Drawing.Size(183, 249)
+        Me.MiControl.TabIndex = 0
+        Me.MiControl.TextoInicial = "Ejemplo"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.MiControl)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents MiControl As ControlUsuario
+End Class
