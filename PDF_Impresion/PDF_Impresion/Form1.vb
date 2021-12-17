1a3b2f507358945566617e084c4251141cd5157e
Natalia Canudas <natalia.200351@hotmail.com>
2021-12-17 10:56:32 +0100

    DIC 17

--- PDF_Impresion/PDF_Impresion/Form1.vb
+++ PDF_Impresion/PDF_Impresion/Form1.vb
@@ -17,13 +17,25 @@
     '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     Friend WithEvents MiPrint As PrintPreviewDialog
+    Friend WithEvents MiPrintDocumento As Drawing.Printing.PrintDocument
+    Friend WithEvents MiImpresora As New PrintDialog
+    'PrintPreviewDialog --> Dialogo que contiene la vista previa de impresión
+    'PrintDocument --> Clase que envia el documento a la impresora y establece la configuración de impresion
+    'PrintDialog --> Permite elegir una impresora y las secciones del documento a imprimir
 
     Private Sub btnImprimir_Click(sender As Object, e As EventArgs) Handles btnImprimir.Click
         MiPrint = New PrintPreviewDialog
+        MiPrintDocumento = New Printing.PrintDocument
 
         'Rellena el documento previo
+        MiPrint.Document = MiPrintDocumento 'Vista previa del documento
+        MiPrintDocumento.PrinterSettings = MiImpresora.PrinterSettings 'Se usa la configuración de impresion definida en la vista previa
+        MiPrintDocumento.DefaultPageSettings.Landscape = True 'Documento apaisado
 
-
-        MiPrint.ShowDialog()
+        If MiPrint.ShowDialog() = DialogResult.OK Then
+            If MiImpresora.ShowDialog() = DialogResult.OK Then
+                MiPrint.Document.Print() 'Se imprime el documento
+            End If
+        End If
     End Sub
 End Class
